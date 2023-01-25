ad95c22f90d26f3fce503a25eb5b994282883ffd
Jonathann09 <jonathanurrete005@gmail.com>
2023-01-25 12:50:23 +0800

    Quick sort

--- Sorting.py
+++ Sorting.py
@@ -94,4 +94,47 @@ def mergeSort(myList):
 mylist = [24, 71, 8, 1, 16, 56, 68, 18, 19, 53] 
 mergeSort(mylist)
    
+   
 
+print("===========================")
+print("        QUICK SORT         ")
+print("===========================")
+
+#QUICK SORT
+
+
+def quickSort(array, left, right):
+     if left < right:
+        pi = partition(array, left, right)
+        
+        quickSort(array, left, pi - 1)
+        
+        quickSort(array, pi + 1, right)
+        
+
+def partition(arr, left, right):
+    i = left
+    j = right - 1
+    pivot = arr[right]
+    print("Pivot", pivot)
+    while i < j:
+            while i < right and arr[i] < pivot:
+                 i += 1
+                 print("i", i, "arr(i)",arr[i])
+            while j > left and arr[j] >= pivot:
+                 j -= 1
+                 print("j", j, "arr[j]",arr[j])
+            if i < j:
+                 arr[i], arr[j] = arr[j], arr[i]
+                 print(arr)
+    if arr[i]> pivot:
+           arr[i], arr[right] = arr[right], arr[i]
+           print(arr)
+    return i
+data = [24, 71, 8, 1, 16, 56, 68, 18, 19, 53] 
+print(data)
+quickSort(data, 0, len(data) -1)
+       
+
+
+   
