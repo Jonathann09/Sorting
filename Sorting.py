b80753f36d927fb157ea2a6ec761f3a7138e61b7
Jonathann09 <jonathanurrete005@gmail.com>
2023-01-25 12:35:06 +0800

    Merge Sort

--- Sorting.py
+++ Sorting.py
@@ -53,5 +53,45 @@ array = [24, 71, 8, 1, 16, 56, 68, 18, 19, 53]
 Insertionsort(array)
 
 
+print("===========================")
+print("        MERGE SORT         ")
+print("===========================")
 
+#MERGE SORT
+
+def mergeSort(myList):
+    if len(myList) > 1:
+            mid = len(myList) // 2
+            left = myList[:mid]
+            right = myList[mid:]
+            print("left: ", left, "right: ", right )
+            #recursive call on each half
+            mergeSort(left)
+            mergeSort(right)
+    
+            i = 0
+            j = 0
+            
+            k = 0
+    
+            while i < len(left) and j < len(right):
+              if left[i] <= right[j]:
+               myList[k] = left[j]
+              i += 1
+            else:
+             myList[k] = right[j]
+             j += 1
+             k += 1
+            while i < len(left):
+              myList[k] = left[1]
+              i + 1
+              k += 1        
+            while j < len(right):
+              myList[k]=right[j]
+              j += 1
+              k += 1
+              print(myList)
+mylist = [24, 71, 8, 1, 16, 56, 68, 18, 19, 53] 
+mergeSort(mylist)
+   
 
